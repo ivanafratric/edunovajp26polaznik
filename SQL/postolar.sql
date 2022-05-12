653d54f9896d75338d0b26af73ba07321569e460
Ivana Fratrić <104583385+ivanafratric@users.noreply.github.com>
2022-05-12 20:40:43 +0200

    Postolar

--- SQL/postolar.sql
+++ SQL/postolar.sql
@@ -5,32 +5,51 @@ create database postolar;
 use postolar;
 
 create table postolar(
-    naziv varchar(50),
-    usluga varchar(50),
-    cijena varchar(50),
-    lokacija varchar(50),
-    kontakt varchar(50)
+    sifra int not null primary key auto_increment,
+    naziv varchar(50) not null,
+    popravak int not null,
+    cijena decimal(18,2) not null,
+    lokacija varchar(50) not null,
+    kontakt varchar(50) not null,
+    segrt int not null
 );
 
-create table usluga(
-    zamjenapotplata varchar(50),
-    zamjenauloska varchar(50),
-    zamjenapatenta varchar(50)
+create table popravak(
+    sifra int not null primary key auto_increment,
+    vrstapopravka varchar(50) not null
 );
 
 create table segrt(
-    ime varchar(50),
-    prezime varchar(50),
-    oib varchar(50),
-    kontakt varchar(50),
-    iban varchar(50)
+    sifra int not null primary key auto_increment,
+    ime varchar(20) not null,
+    prezime varchar(20) not null,
+    oib char(11), 
+    kontakt varchar(50) not null,
+    iban varchar(50) not null,
+    popravak int not null
 );
 
 create table korisnik(
-    ime varchar(50),
-    prezime varchar(50),
-    obuca varchar(50),
-    usluga varchar(50),
-    kontakt varchar(50)
+    sifra int not null primary key auto_increment,
+    ime varchar(20) not null,
+    prezime varchar(20) not null,
+    obuca int not null,
+    popravak int not null,
+    kontakt varchar(50) not null
 );
+
+create table obuca(
+    sifra int not null primary key auto_increment,
+    marka varchar(20),
+    popravak int not null
+);
+
+alter table postolar add foreign key (popravak) references popravak(sifra);
+alter table postolar add foreign key (segrt) references segrt(sifra);
+
+alter table segrt add foreign key (popravak) references popravak(sifra);
+
+alter table korisnik add foreign key (obuca) references obuca(sifra);
+alter table korisnik add foreign key (popravak) references popravak(sifra);
+
 
