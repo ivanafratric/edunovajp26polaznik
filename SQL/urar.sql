38feb0c9a04a1b0dfcf501f6e18f937bc46abb0e
Ivana Fratrić <104583385+ivanafratric@users.noreply.github.com>
2022-05-12 20:51:08 +0200

    Urar dodani ključevi

--- SQL/urar.sql
+++ SQL/urar.sql
@@ -5,32 +5,49 @@ create database urar;
 use urar;
 
 create table urar(
-    naziv varchar(50),
-    usluga varchar(50),
-    cijena varchar(50),
-    lokacija varchar(50),
-    kontakt varchar(50)
+    sifra int not null primary key auto_increment,
+    naziv varchar(50) not null,
+    popravak int not null,
+    cijena decimal(18,2) not null,
+    lokacija varchar(50) not null,
+    kontakt varchar(50) not null,
+    segrt int not null
 );
 
-create table usluga(
-    zamjenabaterije varchar(50),
-    zamjenanarukvice varchar(50),
-    zamjenakazaljki varchar(50)
+create table popravak(
+    sifra int not null primary key auto_increment,
+    vrstapopravka varchar(50) not null
 );
 
 create table segrt(
-    ime varchar(50),
-    prezime varchar(50),
-    oib varchar(50),
-    kontakt varchar(50),
-    iban varchar(50)
+    sifra int not null primary key auto_increment,
+    ime varchar(20) not null,
+    prezime varchar(20) not null,
+    oib char(11),
+    kontakt varchar(50) not null,
+    iban varchar(50) not null,
+    popravak int not null
 );
 
 create table korisnik(
-    ime varchar(50),
-    prezime varchar(50),
-    sat varchar(50),
-    usluga varchar(50),
-    kontakt varchar(50)
+    sifra int not null primary key auto_increment,
+    ime varchar(20) not null,
+    prezime varchar(20) not null,
+    sat int not null,
+    popravak int not null,
+    kontakt varchar(50) not null
 );
 
+create table sat(
+    sifra int not null primary key auto_increment,
+    marka varchar(20),
+    popravak int not null
+);
+
+alter table urar add foreign key (popravak) references popravak(sifra);
+alter table urar add foreign key (segrt) references segrt(sifra);
+
+alter table segrt add foreign key (popravak) references popravak(sifra);
+
+alter table korisnik add foreign key (sat) references sat(sifra);
+alter table korisnik add foreign key (popravak) references popravak(sifra);
