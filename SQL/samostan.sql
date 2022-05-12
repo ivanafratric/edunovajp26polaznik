7c7f6b3b45f5c1abb54b0ea5a2b3591547e57e62
Ivana Fratrić <104583385+ivanafratric@users.noreply.github.com>
2022-05-12 19:41:44 +0200

    Samostan

--- SQL/samostan.sql
+++ SQL/samostan.sql
@@ -1,14 +1,15 @@
-# C:\xampp\mysql\bin\mysql -uroot < C:\Users\38591\Documents\github\edunovajp26polaznik\SQL\6tablica.sql
+# C:\xampp\mysql\bin\mysql -uroot < C:\Users\38591\Documents\github\edunovajp26polaznik\SQL\samostan.sql
 
-drop database if exists 6tablica;
-create database 6tablica;
-use 6tablica;
+drop database if exists samostan;
+create database samostan;
+use samostan;
 
 create table samostan(
     ime varchar(50),
     red varchar(50),
     lokacija varchar(50),
     svecenik varchar(50)
+    nadredenisvecenik varchar(50)
 );
 
 create table svecenik(
