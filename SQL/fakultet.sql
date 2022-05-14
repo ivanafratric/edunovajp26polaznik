e1848d134017f1e64c3692a2666fc9427bb3f998
Ivana Fratrić <104583385+ivanafratric@users.noreply.github.com>
2022-05-14 14:28:13 +0200

    Fakultet dodani ključevi

--- SQL/fakultet.sql
+++ SQL/fakultet.sql
@@ -5,6 +5,7 @@ create database fakultet;
 use fakultet;
 
 create table fakultet(
+    sifra int not null primary key auto_increment,
     naziv varchar(50) not null,
     lokacija varchar(50) not null,
     kolegij int not null,
@@ -12,6 +13,7 @@ create table fakultet(
 );
 
 create table student(
+    sifra int not null primary key auto_increment,
     ime varchar(20),
     prezime varchar(20),
     oib char(11) not null,
@@ -19,18 +21,29 @@ create table student(
 );
 
 create table kolegij(
+    sifra int not null primary key auto_increment,
     naziv varchar(50) not null,
     profesor varchar(50)
 );
 
 create table rok(
+    sifra int not null primary key auto_increment,
     datum datetime not null,
     vrijeme datetime not null
 );
 
 create table clan(
+    sifra int not null primary key auto_increment,
     student int not null,
     rok int not null
 );
+
+alter table fakultet add foreign key (kolegij) references kolegij(sifra);
+alter table student add foreign key (kolegij) references kolegij(sifra);
+
+alter table clan add foreign key (student) references student(sifra);
+alter table clan add foreign key (rok) references rok(sifra);
+
+
 
 
