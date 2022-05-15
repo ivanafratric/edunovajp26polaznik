4e2ca0ac31cc04bcd6704fe395fc37f9dabed9f9
Ivana Fratrić <104583385+ivanafratric@users.noreply.github.com>
2022-05-15 20:03:23 +0200

    Insert podataka

--- SQL/doktorskaordinacija.sql
+++ SQL/doktorskaordinacija.sql
@@ -1,4 +1,4 @@
-# C:\xampp\mysql\bin\mysql -uroot < C:\Users\38591\Documents\github\edunovajp26polaznik\SQL\doktorskaordinacija.sql
+# C:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\Users\38591\Documents\github\edunovajp26polaznik\SQL\doktorskaordinacija.sql
 
 drop database if exists doktorskaordinacija;
 create database doktorskaordinacija;
@@ -18,7 +18,7 @@ create table doktor(
     sifra int not null primary key auto_increment,
     ime varchar(20) not null,
     prezime varchar(20) not null,
-    oib char(11) not null,
+    oib char(11),
     kontakt varchar(50),
     pacijent int not null
 );
@@ -27,7 +27,7 @@ create table pacijent(
     sifra int not null primary key auto_increment,
     ime varchar(20) not null,
     prezime varchar(20) not null,
-    oib char(11) not null,
+    oib char(11),
     bolest varchar(50),
     kontakt varchar(50)
 );
@@ -36,7 +36,7 @@ create table medicinskasestra(
     sifra int not null primary key auto_increment,
     ime varchar(20) not null,
     prezime varchar(20) not null,
-    oib char(11) not null,
+    oib char(11),
     kontakt varchar(50) not null,
     pacijent int not null
 );
@@ -47,3 +47,21 @@ alter table doktorskaordinacija add foreign key (medicinskasestra) references me
 
 alter table doktor add foreign key (pacijent) references pacijent(sifra);
 alter table medicinskasestra add foreign key (pacijent) references pacijent(sifra);
+
+insert into pacijent (ime, prezime, bolest, kontakt)
+values ('Ivo', 'Ivić', 'Prehlada', '091/8878-654');
+
+insert into pacijent (ime, prezime, bolest, kontakt)
+values ('Ivica', 'Mikić', 'Upala krajnika', '099/6658-123'); 
+
+insert into doktor (ime, prezime, kontakt, pacijent)
+values ('Miro', 'Ćutić', 'miro.cutic@doktor.hr', 1);
+
+insert into doktor (ime, prezime, kontakt, pacijent)
+values ('Miro', 'Ćutić', 'miro.cutic@doktor.hr', 2);
+
+insert into medicinskasestra (ime, prezime, kontakt, pacijent)
+values ('Sanja', 'Radić', 'miro.cutic@doktor.hr', 1);
+
+insert into medicinskasestra (ime, prezime, kontakt, pacijent)
+values ('Iva', 'Mihić', 'miro.cutic@doktor.hr', 2);
