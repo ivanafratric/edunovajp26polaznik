76bed02eb88d89a97959daa4b729da2d35937be4
Ivana Fratrić <104583385+ivanafratric@users.noreply.github.com>
2022-05-21 19:50:30 +0200

    Unos podataka

--- VJEZBE/edukacija.sql
+++ VJEZBE/edukacija.sql
@@ -58,3 +58,87 @@ alter table polaznik add foreign key (osoba) references osoba(sifra);
 
 alter table predavac add foreign key (osoba) references osoba(sifra);
 
+insert into smjer (sifra, naziv, trajanje, cijena, upisnina, certificiran)
+values (null, 'Java programiranje', 130, 7000, 550, true);
+
+insert into smjer (sifra, naziv, trajanje, cijena, upisnina, certificiran)
+values (null, 'PHP programiranje', 130, 7000, 550, false);
+
+insert into smjer (sifra, naziv, trajanje, cijena, upisnina, certificiran)
+values (null, 'Računovodstvo', 100, 5000, 500, null);
+
+insert into grupa (sifra, naziv, smjer, datumpocetka, maksimalnopolaznika, predavac)
+values (null, 'JP26', 1, '2022-04-27', 20, null);
+
+insert into grupa (sifra, naziv, smjer, datumpocetka, maksimalnopolaznika, predavac)
+values (null, 'PP25', 2, '2022-04-27', 20, null);
+
+insert into grupa (sifra, naziv, smjer, datumpocetka, maksimalnopolaznika, predavac)
+values (null, 'Rač20', 3, '2022-04-27', 20, null);
+
+insert into osoba (sifra,ime,prezime,oib,email)
+values (null,'Tomislav','Jakopec',null,'tjakopec@gmail.com');
+
+insert into osoba (sifra,prezime,ime,oib,email) values
+(null,'Županić','Marko',null,'marko.zupanic8@gmail.com'),
+(null,'Borić','Alen',null,'boricalen@live.com'),
+(null,'Ramač','Dino',null,'dinoramac@gmail.com'),
+(null,'Fratrić','Ivana',null,'ivana.fratric1@gmail.com'),
+(null,'Prgić','Mihael',null,'mihaelp243@gmail.com'),
+(null,'Skorup','Marko',null,'mskorup@gmail.com'),
+(null,'Nađ','Filip',null,'filip.nad100@gmail.com'),
+(null,'Kasapović','Vedran',null,'vedran.kasapovic@gmail.com'),
+(null,'Stanić','Ivan',null,'stambaivan@yahoo.com'),
+(null,'Ivan','Olujić',null,'ivan.olujic.os@gmail.com'),
+(null,'Vukašin','Vaščanin',null,'vukasinvascanin0@gmail.com'),
+(null,'Olujić','Ivan',null,'ivan.olujic.os@gmail.com'),
+(null,'Vaščanin','Vukašin',null,'vukasinvascanin0@gmail.com'),
+(null,'Zdelar','Mihael',null,'mzdelar741@gmail.com');
+
+insert into osoba (sifra, ime, prezime, oib, email)
+values (null, 'Ninoslav', 'Fratrić', null, null);
+
+insert into osoba (sifra, ime, prezime, oib, email)
+values (null, 'Mario', 'Markotić', null, null);
+
+insert into osoba (sifra, ime, prezime, oib, email)
+values (null, 'Marija', 'Brkić', null, null);
+
+insert into osoba (sifra, ime, prezime, oib, email)
+values (null, 'Jelena', 'Rogić Leš', null, null);
+
+insert into predavac (sifra, osoba, iban)
+values(null, 1, null);
+
+insert into polaznik (sifra, osoba, brojugovora)
+values (null, 2, null), (null, 3, null), (null, 4, null), (null, 5, null),
+(null, 6, null), (null, 7, null), (null, 8, null), (null, 9, null), (null, 10, null),
+(null, 11, null), (null, 12, null), (null, 13, null), (null, 14, null), (null, 15, null),
+(null, 16, null), (null, 17, null), (null, 18, null), (null, 19, null);
+
+insert into clan (grupa, polaznik)
+values (1, 1), (1,2), (1,3), (1,4), (1,5), (1,6), (1,7), (1,8), (1,9), (1,10), (1,11), (1,12), (1,13), (1,14),
+(1,15), (1,16), (1,17), (1,18);
+
+update smjer set upisnina=600 where sifra=1;
+
+update smjer set cijena=5000, trajanje=100 where sifra=2;
+
+update grupa set predavac=1 where sifra=1;
+
+update smjer set cijena=null where sifra=3;
+
+insert into predavac (sifra, osoba, iban)
+values (null, 5, null);
+
+insert into smjer (sifra, naziv, trajanje, cijena, upisnina, certificiran)
+values (null, 'Tehničar za mrežu', 130, 5000, 550, true);
+
+update predavac set iban='HR15489653321455' where sifra=2;
+
+insert into polaznik (sifra, osoba, brojugovora)
+values (null, 16, null), (null, 17, null), (null, 18, null), (null, 19, null);
+
+insert into clan (grupa, polaznik)
+values (1, 19), (1, 20), (2, 21), (2, 22);
+
