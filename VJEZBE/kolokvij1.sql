799c85fd48a13c67b4222544c27010fda13cd4b4
Ivana Fratrić <104583385+ivanafratric@users.noreply.github.com>
2022-05-24 18:21:27 +0200

    Kolokvij 1.

---
+++ VJEZBE/kolokvij1.sql
@@ -0,0 +1,91 @@
+# C:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\Users\38591\Documents\github\edunovajp26polaznik\VJEZBE\kolokvij1.sql
+
+drop database if exists kolokvij1;
+create database kolokvij1;
+use kolokvij1;
+
+create table sestra(
+    sifra int not null primary key auto_increment,
+    introvertno boolean,
+    haljina varchar(31) not null,
+    maraka decimal(16,6),
+    hlace varchar(46) not null,
+    narukvica int not null
+);
+
+create table svekar(
+    sifra int not null primary key auto_increment,
+    bojaociju varchar(40) not null,
+    prstena int,
+    dukserica varchar(41),
+    lipa decimal(13,8),
+    eura decimal(12,7),
+    majica varchar(35)
+);
+
+create table sestra_svekar(
+    sifra int not null primary key auto_increment,
+    sestra int not null,
+    svekar int not null
+);
+
+create table punac(
+    sifra int not null primary key auto_increment,
+    ogrlica int,
+    gustoca decimal(14,9),
+    hlace varchar(41) not null
+);
+
+create table cura(
+    sifra int not null primary key auto_increment,
+    novcica decimal(16,5) not null,
+    gustoca decimal(18,6) not null,
+    lipa decimal(13,10),
+    ogrlica int not null,
+    bojakose varchar(38),
+    suknja varchar(36),
+    punac int
+);
+
+create table zena(
+    sifra int not null primary key auto_increment,
+    treciputa datetime,
+    hlace varchar(46),
+    kratkamajica varchar(31) not null,
+    jmbag char(11) not null,
+    bojaociju varchar(39) not null,
+    haljina varchar(44),
+    sestra int not null
+);
+
+create table muskarac(
+    sifra int not null primary key auto_increment,
+    bojaociju varchar(50) not null,
+    hlace varchar(30),
+    modelnaocala varchar(43),
+    maraka decimal(14,5) not null,
+    zena int not null
+);
+
+create table mladic(
+    sifra int not null primary key auto_increment,
+    suknja varchar(50) not null,
+    kuna decimal(16,8) not null,
+    drugiputa datetime,
+    asocijalno boolean,
+    ekstroventno boolean not null,
+    dukserica varchar(48) not null,
+    muskarac int not null
+);
+
+alter table sestra_svekar add foreign key (sestra) references sestra(sifra);
+alter table sestra_svekar add foreign key (svekar) references svekar(sifra);
+
+alter table zena add foreign key (sestra) references sestra(sifra);
+
+alter table muskarac add foreign key (zena) references zena(sifra);
+
+alter table mladic add foreign key (muskarac) references muskarac(sifra);
+
+alter table cura add foreign key (punac) references punac(sifra);
+
