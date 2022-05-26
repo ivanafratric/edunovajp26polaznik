7dedd11631bd70076e6c2d91fb280939b6cf51f9
Ivana Fratrić <104583385+ivanafratric@users.noreply.github.com>
2022-05-26 18:47:59 +0200

    Insert podataka

--- VJEZBE/kolokvij2.sql
+++ VJEZBE/kolokvij2.sql
@@ -83,3 +83,47 @@ alter table neprijatelj add foreign key (cura) references cura(sifra);
 
 alter table brat add foreign key (neprijatelj) references neprijatelj(sifra);
 
+insert into neprijatelj (haljina, modelnaocala, kuna)
+values ('curina', 'sunčane', 252.50);
+
+insert into neprijatelj (haljina, modelnaocala, kuna)
+values ('zelena', 'dioptrijske', 485.42);
+
+insert into neprijatelj (haljina, modelnaocala, kuna)
+values ('crna', 'lenonice', 585);
+
+insert into cura (haljina, drugiputa, majica)
+values ('mornarska', '2022-08-25', 'bijela');
+
+insert into cura (haljina, drugiputa, majica)
+values ('bijela', '2018-01-15', 'dugih rukava');
+
+insert into cura (haljina, drugiputa, majica)
+values ('zelena', '1995-10-01', 'žuta');
+
+insert into decko (asocijalno)
+values (true);
+
+insert into decko (asocijalno)
+values (true);
+
+insert into decko (asocijalno)
+values (false);
+
+insert into zarucnica (bojakose, lipa, indiferentno)
+values ('smeđa', 0.15, true);
+
+insert into zarucnica (bojakose, lipa, indiferentno)
+values ('crna', 0.99, true);
+
+insert into zarucnica (bojakose, lipa, indiferentno)
+values ('plava', 0.50, false);
+
+insert into decko_zarucnica (decko, zarucnica)
+values (1, 3);
+
+insert into decko_zarucnica (decko, zarucnica)
+values (2, 2);
+
+insert into decko_zarucnica (decko, zarucnica)
+values (3, 1);
