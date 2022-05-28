d19109706c3670422cefa2a8404109d86e9fc93c
Ivana Fratrić <104583385+ivanafratric@users.noreply.github.com>
2022-05-28 20:18:50 +0200

    Kolokvij 3.

---
+++ VJEZBE/kolokvij3.sql
@@ -0,0 +1,84 @@
+# C:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\Users\38591\Documents\github\edunovajp26polaznik\VJEZBE\kolokvij3.sql
+
+drop database if exists kolokvij3;
+create database kolokvij3;
+use kolokvij3;
+
+create table cura(
+    sifra int not null primary key auto_increment,
+    dukserica varchar(49),
+    maraka decimal (13,7),
+    drugiputa datetime,
+    majica varchar(49),
+    novcica decimal (15,8),
+    ogrlica int not null
+);
+
+create table svekar(
+    sifra int not null primary key auto_increment,
+    novcica decimal(16,8) not null,
+    suknja varchar(44) not null,
+    bojakose varchar(36),
+    prstena int,
+    narukvica int not null,
+    cura int not null
+);
+
+create table brat(
+    sifra int not null primary key auto_increment,
+    jmbag char(11),
+    ogrlica int not null,
+    ekstroventno boolean not null
+);
+
+create table prijatelj(
+    sifra int not null primary key auto_increment,
+    kuna decimal(16,1),
+    haljina varchar(37),
+    lipa decimal(13,10),
+    dukserica varchar(31),
+    indiferentno boolean not null
+);
+
+create table prijatelj_brat(
+    sifra int not null primary key auto_increment,
+    prijatelj int not null,
+    brat int not null
+);
+
+create table ostavljena(
+    sifra int not null primary key auto_increment,
+    kuna decimal(17,5),
+    lipa decimal(15,6),
+    majica varchar(36),
+    modelnaocala varchar(31) not null,
+    prijatelj int
+);
+
+create table snasa(
+    sifra int not null primary key auto_increment,
+    introvertno boolean,
+    kuna decimal(15,6) not null,
+    eura decimal(12,9) not null,
+    treciputa datetime,
+    ostavljena int not null
+);
+
+create table punica(
+    sifra int not null primary key auto_increment,
+    asocijalno boolean,
+    kratkamajica varchar(44),
+    kuna decimal(13,8) not null,
+    vesta varchar(32) not null,
+    snasa int
+);
+
+alter table svekar add foreign key (cura) references cura(sifra);
+
+alter table prijatelj_brat add foreign key (prijatelj) references prijatelj(sifra);
+alter table prijatelj_brat add foreign key (brat) references brat(sifra);
+
+alter table ostavljena add foreign key (prijatelj) references prijatelj(sifra);
+alter table snasa add foreign key (ostavljena) references ostavljena(sifra);
+alter table punica add foreign key (snasa) references snasa(sifra);
+
