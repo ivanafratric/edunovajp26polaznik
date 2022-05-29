8357a32e7ca8d5b8f6f542d06f4c3df04aac9e2b
Ivana Fratrić <104583385+ivanafratric@users.noreply.github.com>
2022-05-29 17:51:55 +0200

    Kolokvij 4.

---
+++ VJEZBE/kolokvij4.sql
@@ -0,0 +1,72 @@
+# C:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\Users\38591\Documents\github\edunovajp26polaznik\VJEZBE\kolokvij4.sql
+
+drop database if exists kolokvij4;
+create database kolokvij4;
+use kolokvij4;
+
+create table ostavljen(
+    sifra int not null primary key auto_increment,
+    modelnaocala varchar(43),
+    introvertno boolean,
+    kuna decimal(14,10)
+);
+
+create table punac(
+    sifra int not null primary key auto_increment,
+    treciputa datetime,
+    majica varchar(46),
+    jmbag char(11) not null,
+    novcica decimal(18,7) not null,
+    maraka decimal(12,6) not null,
+    ostavljen int not null
+);
+
+create table mladic(
+    sifra int not null primary key auto_increment,
+    kuna decimal(15,9),
+    lipa decimal(18,5),
+    nausnica int,
+    stilfrizura varchar(49),
+    vesta varchar(34) not null
+);
+
+create table zena(
+    sifra int not null primary key auto_increment,
+    suknja varchar(39) not null,
+    lipa decimal(18,7),
+    prstena int not null
+);
+
+create table zena_mladic(
+    sifra int not null primary key auto_increment,
+    zena int not null,
+    mladic int not null
+);
+
+create table snasa(
+    sifra int not null primary key auto_increment,
+    introvertno boolean,
+    treciputa datetime,
+    haljina varchar(44) not null,
+    zena int not null
+);
+
+create table becar(
+    sifra int not null primary key auto_increment,
+    novcica decimal(14,8),
+    kratkamajica varchar(48) not null,
+    bojaociju varchar(36) not null,
+    snasa int not null
+);
+
+
+create table prijatelj(
+    sifra int not null primary key auto_increment,
+    eura decimal(16,9),
+    prstena int not null,
+    gustoca decimal(16,5),
+    jmbag char(11) not null,
+    suknja varchar(47) not null,
+    becar int not null
+);
+
