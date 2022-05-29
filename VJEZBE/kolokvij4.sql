95e0d16b2152dd1d09d6c52000d1ecfb3f6b3e82
Ivana Fratrić <104583385+ivanafratric@users.noreply.github.com>
2022-05-29 18:35:41 +0200

    Kolokvij 4. riješeni svi zadaci

--- VJEZBE/kolokvij4.sql
+++ VJEZBE/kolokvij4.sql
@@ -79,3 +79,74 @@ alter table snasa add foreign key (zena) references zena(sifra);
 alter table becar add foreign key (snasa) references snasa(sifra);
 alter table prijatelj add foreign key (becar) references becar(sifra);
 
+insert into zena (suknja, prstena)
+values ('crna', 4);
+
+insert into zena (suknja, prstena)
+values ('plava', 8);
+
+insert into zena (suknja, prstena)
+values ('zeleno-žuta', 1);
+
+insert into snasa (haljina, zena, treciputa)
+values ('dugačka', 2, '2022-04-15');
+
+insert into snasa (haljina, zena, treciputa)
+values ('kratka', 1, '1999-09-09');
+
+insert into snasa (haljina, zena)
+values ('prugasta', 3);
+
+insert into becar (kratkamajica, bojaociju, snasa)
+values ('plava', 'plava', 1);
+
+insert into becar (kratkamajica, bojaociju, snasa)
+values ('crna', 'zelena', 2);
+
+insert into becar (kratkamajica, bojaociju, snasa)
+values ('bijela', 'smeđa', 3);
+
+insert into mladic (vesta, nausnica)
+values ('topla', 52);
+
+insert into mladic (vesta, nausnica)
+values ('moderna', 5);
+
+insert into mladic (vesta, nausnica)
+values ('tanka', 8);
+
+insert into zena_mladic (zena, mladic)
+values (1, 2);
+
+insert into zena_mladic (zena, mladic)
+values (2,1);
+
+insert into zena_mladic (zena, mladic)
+values (3,3);
+
+insert into prijatelj (prstena, jmbag, suknja, becar)
+values (5, '14587965233', 'zelena', 1);
+
+insert into prijatelj (prstena, jmbag, suknja, becar)
+values (12, '77854996321', 'kratka', 2);
+
+insert into prijatelj (prstena, jmbag, suknja, becar)
+values (7, '11445588231', 'plava', 3);
+
+update punac set majica='Osijek';
+
+delete from prijatelj where prstena > 17;
+
+select haljina from snasa where treciputa is null;
+
+select a.nausnica, b.jmbag, c.kratkamajica
+from mladic a inner join zena_mladic zm on a.sifra=zm.mladic 
+inner join zena d on d.sifra=zm.zena 
+inner join snasa e on e.zena=d.sifra 
+inner join becar c on c.snasa=e.sifra 
+left join prijatelj b on b.becar=c.sifra
+where e.treciputa is not null and d.lipa <> 29;
+
+select a.lipa, a.prstena from zena a
+inner join zena_mladic b on a.sifra=b.zena 
+where b.sifra is null;
