5f056659c67d093d7893f14c47353131b21b90ed
Ivana Fratrić <104583385+ivanafratric@users.noreply.github.com>
2022-05-31 20:01:15 +0200

    Kolokvij 7.  riješeni svi zadaci

--- VJEZBE/kolokvij7.sql
+++ VJEZBE/kolokvij7.sql
@@ -81,4 +81,61 @@ create table punica(
  alter table ostavljen add foreign key (zarucnik) references zarucnik(sifra);
  alter table prijateljica add foreign key (ostavljen) references ostavljen(sifra);
  alter table sestra add foreign key (prijateljica) references prijateljica(sifra);
+
+ insert into prijateljica (gustoca)
+ values (7.5);
+
+ insert into prijateljica (gustoca)
+ values (22.8);
+
+ insert into prijateljica (gustoca)
+ values (150);
+
+ insert into ostavljen (introvertno, kratkamajica, prstena)
+ values (1, 'plava', 4);
  
+ insert into ostavljen (introvertno, kratkamajica, prstena)
+ values (1, 'zelena', 44);
+ 
+ insert into ostavljen (introvertno, kratkamajica, prstena)
+ values (0, 'ljubičasta', 12);
+
+ insert into zarucnik (asocijalno, narukvica, novcica)
+ values (1, 8, 22.22);
+
+ insert into zarucnik (asocijalno, narukvica, novcica)
+ values (0, 22, 155);
+
+  insert into zarucnik (asocijalno, narukvica, novcica)
+ values (0, 56, 148.78);
+
+ insert into mladic (lipa, narukvica, drugiputa)
+ values (15, 15, '1994-01-01');
+
+ insert into mladic (lipa, narukvica, drugiputa)
+ values (22, 14, '2004-01-29');
+
+ insert into mladic (lipa, narukvica, drugiputa)
+ values (99, 3, '1997-09-21');
+
+ insert into zarucnik_mladic (mladic, zarucnik)
+ values (1,1), (2,3), (1,3);
+
+ update punica set eura=15.77;
+
+delete from sestra where hlace < 'AB';
+
+select kratkamajica from ostavljen where introvertno is null;
+
+select a.narukvica, b.stilfrizura, c.gustoca
+from mladic a inner join zarucnik_mladic zm on a.sifra=zm.mladic 
+inner join zarucnik d on zm.zarucnik=d.sifra 
+inner join ostavljen e on e.zarucnik=d.sifra 
+inner join prijateljica c on c.ostavljen=e.sifra 
+inner join sestra b on b.prijateljica=c.sifra
+where e.introvertno is not null and d.asocijalno is not null 
+order by c.gustoca desc;
+
+select a.asocijalno, a.modelnaocala from zarucnik a 
+inner join zarucnik_mladic b on a.sifra=b.zarucnik 
+where b.sifra is null;
